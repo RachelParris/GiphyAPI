409480a2325c9080b9a7e54b092d98db833f0b80
rachelparris <sonnet22@gmail.com>
2017-09-17 15:23:39 -0400

    Add random number generator to API request. More CSS styling.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$('.search-btn').click(function(event) {
+$('#search-btn').click(function(event) {
 	event.preventDefault();
 
 	// Stores the user's input.
@@ -12,63 +12,70 @@ $('.search-btn').click(function(event) {
 
 
 $('.btn').click(function() {
-	console.log('It works!');
-	var apiTrigger = $(this).attr('data-input');
-	console.log(apiTrigger);
+	var trigger = $(this).attr('data-input');
+	callSearch(trigger);
+});
 
-	// Setting up API call.
-	var url = 'http://api.giphy.com/v1/gifs/search?limit=10&q=' + apiTrigger,
-			apiKey = '&api_key=22e070ef831b40b492b87d3a49438799',
-			param = '';
+//Executes the API call.
+function callSearch(apiRequest) {
+	var randomNum = Math.floor(Math.random() * 1000);
 
+		// Setting up API call.
+	var queryURL = 'http://api.giphy.com/v1/gifs/search?limit=10&offset=' + randomNum + '&q=' + apiRequest,
+			apiKey = '&api_key=22e070ef831b40b492b87d3a49438799';
 
 	$.ajax({
-		url: url + apiKey,
+		url: queryURL + apiKey,
 		method: 'GET'
 	}).done(function(response) {
 		console.log('Success!');
+		postGifs(response);
 
-		var result = response.data;
-
-		for (var i = 0; i < result.length; i++) {
-				var imgStill = result[i].images.fixed_height_still.url,
-					imgAnimated = result[i].images.fixed_height.url;
-					link = result[i].url,
-					rating = 'Rated: ' + result[i].rating.toUpperCase();
-
-			var gifDiv = $('<div>'),
-					par = $('<p>').text(rating),
-					img = $('<img>').attr({'src': imgStill, 'data-state': 'still'});
-
-			// var hyperlink = '<a href=' + link + ' targert="_blank">' + link + '</a>';
-
-			gifDiv.prepend(par);
-			gifDiv.prepend(img);
-
-			$('#gifs').prepend(gifDiv);
-
-
-			// Toggle between still and animated states of image.
-			$('img').click(function() {
-				var state = $(this).attr("data-state");
-
-				if ( state === "still") {
-					$(this).attr({'data-state': 'animate'});
-					$(this).attr("src", imgAnimated);
-				} else {
-					$(this).attr({'data-state': 'still'});
-					$(this).attr("src", imgStill);
-				}
-			});
-		}
 	}).fail(function(err) {
 		console.log('API call failed.');
 	})
+}
 
-		
 
+function postGifs(response) {
+	var result = response.data;
+
+	for (var i = 0; i < result.length; i++) {
+		var imgStill = result[i].images.fixed_height_still.url,
+			imgAnimated = result[i].images.fixed_height.url;
+			link = result[i].url,
+			rating = 'Rated: ' + result[i].rating.toUpperCase();
+
+		var gifDiv = $('<div class="item">'),
+			par = $('<p>').text(rating),
+			img = $('<img>').attr({'src': imgStill, 'data-state': 'still'});
+
+		// var hyperlink = '<a href=' + link + ' targert="_blank">' + link + '</a>';
+
+		gifDiv.prepend(par);
+		gifDiv.prepend(img);
+
+		$('#gifs').prepend(gifDiv);
+	}
+
+// Toggle between still and animated states of image.
+$('img').click(function() {
+	var state = $(this).attr("data-state");
+
+	if ( state === "still") {
+		$(this).attr({'data-state': 'animate'});
+		$(this).attr("src", imgAnimated);
+	} else {
+		$(this).attr({'data-state': 'still'});
+		$(this).attr("src", imgStill);
+	}
 });
+	
+}
 
 
 
+function toggleGifs() {
+
+}
 
