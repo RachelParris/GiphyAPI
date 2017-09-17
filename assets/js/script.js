bfdd2ff6dd4756fc470eea366504fa746a7940fe
rachelparris <sonnet22@gmail.com>
2017-09-17 13:07:09 -0400

    Created dynamic buttons, for loop, and gif toogle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,74 @@
-$('button').click(function(event) {
+$('.search-btn').click(function(event) {
 	event.preventDefault();
-	$(this).attr()
+
+	// Stores the user's input.
+	userInput = $('#search-field').val();
+	console.log(userInput);
+
+	// Creates a dynamic button in the search-term div.
+	var createdBtn = $('<button>').attr({'class': 'btn', 'data-input': userInput}).html(userInput);
+	createdBtn.appendTo('#search-terms');
 });
 
 
-// Setting up API call.
-var url = 'http://api.giphy.com/v1/gifs/search?&q=' + 'beyonce',
-		apiKey = '&api_key=22e070ef831b40b492b87d3a49438799&limit=10',
-		param = '';
+$('.btn').click(function() {
+	console.log('It works!');
+	var apiTrigger = $(this).attr('data-input');
+	console.log(apiTrigger);
+
+	// Setting up API call.
+	var url = 'http://api.giphy.com/v1/gifs/search?limit=10&q=' + apiTrigger,
+			apiKey = '&api_key=22e070ef831b40b492b87d3a49438799',
+			param = '';
 
 
-$.ajax({
-	url: url + apiKey,
-	method: 'GET'
-}).done(function(response) {
-	console.log('Success!');
+	$.ajax({
+		url: url + apiKey,
+		method: 'GET'
+	}).done(function(response) {
+		console.log('Success!');
 
-	var result = response.data;
-	console.log(result);
+		var result = response.data;
 
-	for (var i = 0; i < result.length; i++) {
+		for (var i = 0; i < result.length; i++) {
+				var imgStill = result[i].images.fixed_height_still.url,
+					imgAnimated = result[i].images.fixed_height.url;
+					link = result[i].url,
+					rating = 'Rated: ' + result[i].rating.toUpperCase();
 
-		var imgStill = result[i].images.fixed_height_still.url,
-				imgAnimated = result[i].images.fixed_height.url;
-				link = result[i].url,
-				rating = 'Rated: ' + result[i].rating.toUpperCase();
+			var gifDiv = $('<div>'),
+					par = $('<p>').text(rating),
+					img = $('<img>').attr({'src': imgStill, 'data-state': 'still'});
 
-		var gifDiv = $('<div>'),
-				par = $('<p>').text(rating),
-				img = $('<img>').attr({'src': imgStill, 'data-still': imgStill, 'data-animate': imgAnimated});
+			// var hyperlink = '<a href=' + link + ' targert="_blank">' + link + '</a>';
 
-		// var hyperlink = '<a href=' + link + ' targert="_blank">' + link + '</a>';
+			gifDiv.prepend(par);
+			gifDiv.prepend(img);
 
-		gifDiv.prepend(par);
-		gifDiv.prepend(img);
-		$('#gifs').prepend(gifDiv z);
-	}
+			$('#gifs').prepend(gifDiv);
 
 
-	// Toggle between still and animated states of image.
-	$('img').click(function() {
-		var state = $(this).attr("data-state");
+			// Toggle between still and animated states of image.
+			$('img').click(function() {
+				var state = $(this).attr("data-state");
 
-		if ( state === "still") {
-			// $(this).attr('data-animate');
-			// $(this).attr("src", imgAnimated);
-		} else {
-			$(this).attr({'data-state': 'still'});
-			$(this).attr("src", imgStill);
+				if ( state === "still") {
+					$(this).attr({'data-state': 'animate'});
+					$(this).attr("src", imgAnimated);
+				} else {
+					$(this).attr({'data-state': 'still'});
+					$(this).attr("src", imgStill);
+				}
+			});
 		}
-	});
-}).fail(function(err) {
-	console.log('API call failed.');
-})
+	}).fail(function(err) {
+		console.log('API call failed.');
+	})
+
+		
+
+});
+
+
+
+
