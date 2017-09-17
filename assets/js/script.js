e9fa7eb9c83d74b090ec71cac00b8e1f21109e6f
rachelparris <sonnet22@gmail.com>
2017-09-17 01:39:18 -0400

    Bugs from introducing for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-console.log('hello world');
-
 $('button').click(function(event) {
 	event.preventDefault();
 	$(this).attr()
@@ -7,8 +5,8 @@ $('button').click(function(event) {
 
 
 // Setting up API call.
-var url = 'http://api.giphy.com/v1/gifs/search?q=' + 'beyonce',
-		apiKey = '&api_key=22e070ef831b40b492b87d3a49438799',
+var url = 'http://api.giphy.com/v1/gifs/search?&q=' + 'beyonce',
+		apiKey = '&api_key=22e070ef831b40b492b87d3a49438799&limit=10',
 		param = '';
 
 
@@ -18,23 +16,26 @@ $.ajax({
 }).done(function(response) {
 	console.log('Success!');
 
-	var imgStill = response.data[0].images.fixed_height_still.url,
-			imgAnimated = response.data[0].images.fixed_height.url;
-			link = response.data[0].url,
-			rating = 'Rated: ' + response.data[0].rating.toUpperCase();
+	var result = response.data;
+	console.log(result);
 
-	var gifDiv = $('<div>'),
-			par = $('<p>').text(rating),
-			img = $('<img>').attr({'src': imgStill, 'data-state': 'still'}),
-			imgA = $('<img>').attr('src', imgAnimated);
+	for (var i = 0; i < result.length; i++) {
 
-	// var hyperlink = '<a href=' + link + ' targert="_blank">' + link + '</a>';
+		var imgStill = result[i].images.fixed_height_still.url,
+				imgAnimated = result[i].images.fixed_height.url;
+				link = result[i].url,
+				rating = 'Rated: ' + result[i].rating.toUpperCase();
 
-	gifDiv.prepend(par);
-	gifDiv.prepend(img);
-	// gifDiv.prepend(imgA);
+		var gifDiv = $('<div>'),
+				par = $('<p>').text(rating),
+				img = $('<img>').attr({'src': imgStill, 'data-still': imgStill, 'data-animate': imgAnimated});
 
-	$('#gifs').prepend(gifDiv);
+		// var hyperlink = '<a href=' + link + ' targert="_blank">' + link + '</a>';
+
+		gifDiv.prepend(par);
+		gifDiv.prepend(img);
+		$('#gifs').prepend(gifDiv z);
+	}
 
 
 	// Toggle between still and animated states of image.
@@ -42,8 +43,8 @@ $.ajax({
 		var state = $(this).attr("data-state");
 
 		if ( state === "still") {
-			$(this).attr({'data-state': 'animate'});
-			$(this).attr("src", imgAnimated);
+			// $(this).attr('data-animate');
+			// $(this).attr("src", imgAnimated);
 		} else {
 			$(this).attr({'data-state': 'still'});
 			$(this).attr("src", imgStill);
